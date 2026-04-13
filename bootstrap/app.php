5ffe94939a183329cc7eca407e52072c4f59cc15
Einha Alliah Muday <aynamuday@gmail.com>
2026-04-13 19:45:32 +0800

    minor updates

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,9 +8,9 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        channels: __DIR__.'/../routes/channels.php',
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
-        // channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
     ->withBroadcasting(
@@ -23,5 +23,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        
     })->create();
